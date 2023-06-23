1a049de3b084869526184171a46bff961f075063
ah-moazzeni <am.h0.moazzeni@gmail.com>
2023-06-23 15:54:42 +0330

    add trie and node

--- src/main/java/Dictionary/Main.java
+++ src/main/java/Dictionary/Main.java
@@ -1,4 +1,5 @@
 package Dictionary;
 
 public class Main {
+
 }
---
+++ src/main/java/Dictionary/Node.java
@@ -0,0 +1,39 @@
+package Dictionary;
+import java.util.ArrayList;
+import java.util.*;
+public class Node {
+    private Character letter;
+    ArrayList<Node>letters =new ArrayList<Node>(Collections.nCopies(26, null));
+    private Node father;
+    private Boolean finalLetter;
+
+    public Character getLetter() {
+        return letter;
+    }
+
+    public Node getFather() {
+        return father;
+    }
+
+    public Boolean getFinalLetter() {
+        return finalLetter;
+    }
+
+    public void setLetters(ArrayList<Node> letters) {
+        this.letters = letters;
+    }
+
+    public void setFather(Node father) {
+        this.father = father;
+    }
+
+    public void setFinalLetter(Boolean finalLetter) {
+        this.finalLetter = finalLetter;
+    }
+
+    public Node(Character letter, Boolean finalLetter, Node father) {
+        this.letter = letter;
+        this.finalLetter = finalLetter;
+        this.father=father;
+    }
+}
