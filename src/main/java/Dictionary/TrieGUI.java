c7c878d3f40f64df575e0207e60ce24c22fec8b0
mrn071 <mohammadrezanaziri0@gmail.com>
2023-06-29 14:20:45 +0330

    connect back and front and modify gui

--- src/main/java/Dictionary/TrieGUI.java
+++ src/main/java/Dictionary/TrieGUI.java
@@ -6,9 +6,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class TrieGUI {
     public static void mainScene(Stage stage){
@@ -28,35 +32,41 @@ public class TrieGUI {
         TextField wordTextField = new TextField();
         wordTextField.setTooltip(new Tooltip("Enter your word..."));
 
+        Label tempLabel = new Label();
+
         ////////////////////////////
         wordTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            newValue = newValue.toLowerCase();
             int temp = Main.originalTrie.isExist(newValue);
-            System.out.println(temp);
             if(temp == 0){
-//                AutoComplete.getLastNode("appl");
-//                System.out.println(AutoComplete.PossibleWords);
-//                System.out.println(AutoComplete.PrioritizeWords);
+//                System.out.println("phase 3: " + Correction.correctionSuggestion(newValue));
+                tempLabel.setText(ListToString(Correction.correctionSuggestion(newValue), temp));
                 scene.getStylesheets().clear();
                 scene.getStylesheets().add("styleError.css");
                 //phase3
             }else if(temp == 1){
-//                AutoComplete.getLastNode("appl");
-//                System.out.println(AutoComplete.PossibleWords);
-//                System.out.println(AutoComplete.PrioritizeWords);
+                tempLabel.setText("");
+                if(!newValue.equals("")) {
+                    AutoComplete.getLastNode(newValue);
+//                    System.out.println(AutoComplete.PossibleWords);
+//                    System.out.println(AutoComplete.PrioritizeWords);
+//                    System.out.println(AutoComplete.fiveRecentWords());
+                    tempLabel.setText(ListToString(AutoComplete.fiveRecentWords(), temp));
+                }
                 scene.getStylesheets().clear();
                 //phase2
             }else if(temp == 2){
-//                AutoComplete.getLastNode("appl");
+                AutoComplete.getLastNode(newValue);
 //                System.out.println(AutoComplete.PossibleWords);
 //                System.out.println(AutoComplete.PrioritizeWords);
+//                System.out.println(AutoComplete.fiveRecentWords());
+                tempLabel.setText(ListToString(AutoComplete.fiveRecentWords(), temp));
                 scene.getStylesheets().clear();
                 scene.getStylesheets().add("styleSuccess.css");
                 //phase2
             }
-            System.out.println("textfield changed from " + oldValue + " to " + newValue);
+//            System.out.println("textfield changed from " + oldValue + " to " + newValue);
         });
-
-        Label tempLabel = new Label();
 
         /*
         Button temp = new Button("Don't Click");
@@ -86,6 +96,8 @@ public class TrieGUI {
         wordHBox.setAlignment(Pos.CENTER);
         wordHBox.setSpacing(20);
 
+        root.setStyle("-fx-font-size: 18;");
+
 
         root.getChildren().setAll(wordHBox, tempLabel);
 //        root.getChildren().setAll(wordHBox, submitButton, tempLabel, temp);
@@ -93,5 +105,19 @@ public class TrieGUI {
         root.setSpacing(15);
 
         stage.setScene(scene);
+    }
+
+    public static String ListToString(ArrayList<String> src, int mode){
+        String result = "";
+        if(mode == 1 || mode == 2)
+            result += "Suggestions:\n";
+        else if(mode == 0)
+            result += "Correction Suggestions:\n";
+        if(src.size() == 0)
+            result += "There is no suggestion:(\n";
+        else
+            for(int i=1;i <= src.size();i++)
+                result += i + ". " + src.get(i-1) + "\n";
+        return result;
     }
 }
